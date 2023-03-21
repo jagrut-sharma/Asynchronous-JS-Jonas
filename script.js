29abebeb1a5294d31c41ca8f8411902fa923d765
Jagrut Sharma <jagrut.sharma1@gmail.com>
2023-03-21 15:44:47 +0530

    rendered using promises

--- script.js
+++ script.js
@@ -5,7 +5,70 @@ const countriesContainer = document.querySelector('.countries');
 
 ///////////////////////////////////////
 
+// FETCH API:
+
+// const request = fetch(`https://restcountries.com/v3.1/name/bharat`);
+// console.log(request);
+
 const renderCountry = function (data, className = '') {
+  const html = `
+  <article class="country ${className}">
+  <img class="country__img" src="${data.flags.svg}" />
+  <div class="country__data">
+    <h3 class="country__name">${data.name.official}</h3>
+    <h4 class="country__region">${data.region}</h4>
+    <p class="country__row"><span>👫</span>${(
+      +data.population / 1000000
+    ).toFixed(1)}</p>
+    <p class="country__row"><span>🗣️</span>${
+      data.languages[Object.keys(data.languages)[0]]
+    }</p>
+    <p class="country__row"><span>💰</span>${
+      data.currencies[Object.keys(data.currencies)[0]].name
+    }</p>
+  </div>
+</article>
+  `;
+
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+  countriesContainer.style.opacity = 1;
+};
+
+// const getCountryData = function (country) {
+//   fetch(`https://restcountries.com/v3.1/name/${country}`)
+//     .then(function (res) {
+//       console.log(res);
+//       return res.json();
+//     })
+//     .then(function (data) {
+//       console.log(data);
+//       renderCountry(data[0]);
+//     });
+// };
+
+const getCountryData = function (country) {
+  fetch(`https://restcountries.com/v3.1/name/${country}`)
+    .then(
+      res => res.json(),
+      err => alert(err)
+    )
+    .then(data => {
+      console.log(data[0]);
+      renderCountry(data[0]);
+      const neighbour = data[0].borders?.[0];
+      // console.log(neighbour);
+
+      // getting data of neighbor
+      return fetch(`https://restcountries.com/v3.1/alpha/${neighbour}`);
+    })
+    .then(res => res.json())
+    .then(data => renderCountry(data[0], 'neighbour'));
+};
+
+getCountryData('Bharat');
+
+/* 
+  const renderCountry = function (data, className = '') {
   const html = `
   <article class="country ${className}">
   <img class="country__img" src="${data.flags.svg}" />
@@ -70,3 +133,4 @@ getCountryAndNeighbour('Bharat');
 // getCountryAndNeighbour('Maldives'); // testing for countries with no border
 // getCountryData('Israel');
 // getCountryData('australia');
+ */
